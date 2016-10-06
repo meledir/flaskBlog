e15f94a4dec17465f3c9609ee88bc42aa85ac6b3
Meledir <kinn-lai@list.ru>
2016-10-06 15:49:47 +0500

    try dif routes

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask 
 app = Flask(__name__)
 
+
 @app.route('/')
+def index():
+  return 'Index page here!'
+
+@app.route('/hi')
 def hi_world():
   return 'Hello, world!'
+
+@app.route('/calc/sum/<int:arg1>/<int:arg2>')
+def calc_sum(arg1, arg2):
+  result = arg1 + arg2
+  return 'Result = %d' % result
