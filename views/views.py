f79a6fbc7b12705dad07bafd2e7a1e450d6a7c6e
Meledir <kinn-lai@list.ru>
2016-10-06 18:15:58 +0500

    add base validation of login form

--- views/views.py
+++ views/views.py
@@ -34,4 +34,7 @@ def calc_sum(arg1, arg2):
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for OpenId='+form.openid.data+', remember me='+ str(form.remember_me.data))
+    return redirect('/index')
   return render_template('login.html',title = 'Sign In', form = form)
